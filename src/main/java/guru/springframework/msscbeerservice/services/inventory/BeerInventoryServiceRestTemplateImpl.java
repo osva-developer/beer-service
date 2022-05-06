c777bb73f1808aa84a794e23ac81f22fdee78bd9
osva-developer <oswaldomoralesperez94@gmail.com>
2022-05-05 19:19:43 -0500

    client feign was added but i need to add come classes of before videos

--- src/main/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
+++ src/main/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
@@ -1,26 +1,28 @@
 package guru.springframework.msscbeerservice.services.inventory;
 
-import guru.springframework.msscbeerservice.services.inventory.model.BeerInventoryDto;
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
+import guru.springframework.msscbeerservice.services.inventory.model.BeerInventoryDto;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Profile("!local-discovery")
 @Slf4j
 @ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
 @Component
 public class BeerInventoryServiceRestTemplateImpl implements BeerInventoryService {
 
-    private final String INVENTORY_PATH = "/api/v1/beer/{beerId}/inventory";
+    public static final String INVENTORY_PATH = "/api/v1/beer/{beerId}/inventory";
     private final RestTemplate restTemplate;
 
     private String beerInventoryServiceHost;
@@ -50,6 +52,7 @@ public class BeerInventoryServiceRestTemplateImpl implements BeerInventoryServic
                 .sum();
         
         log.debug("the number is:"+onHand);
+        log.debug("size list is:"+responseEntity.getBody().size());
 
         return onHand;
     }
