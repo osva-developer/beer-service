0046a8e96900ae80260e61a35dfe4e8b6906f6ce
osva-developer <oswaldomoralesperez94@gmail.com>
2022-05-08 18:28:15 -0500

    update:beer service

--- src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
+++ src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
@@ -13,17 +13,18 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 @Slf4j
 public class JmsConfig {
-  
-	public static final String BREWING_REQUEST_QUEUE = "brewing-request";
-	public static final String NEW_INVENTORY_QUEUE ="new-inventory";
-	
-    @Bean //it bean is to serialize message content to json using TextMessage
+
+    public static final String BREWING_REQUEST_QUEUE = "brewing-request";
+    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
+    public static final String VALIDATE_ORDER_RESPONSE_QUEUE = "validate-order-response";
+
+    @Bean // Serialize message content to json using TextMessage
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
         converter.setObjectMapper(objectMapper);
-        log.info("THIS IS JMS CONFIG CLASS");
         return converter;
-    } 
+    }
 }
